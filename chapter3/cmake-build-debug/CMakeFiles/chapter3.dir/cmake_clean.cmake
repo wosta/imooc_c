69abd7c75acc9a93de2d794defb7c06499eba114
smrz1202 <olivepeterwei@gmail.com>
2021-07-18 15:37:08 +0800

    add: chapter3

---
+++ chapter3/cmake-build-debug/CMakeFiles/chapter3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter3.dir/main.c.obj"
+  "chapter3.exe"
+  "chapter3.exe.manifest"
+  "chapter3.pdb"
+  "libchapter3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
